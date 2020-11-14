96a2894a6c8c092f7e157e98c727d93cea5434de
niki <niki@theliferocks.com>
2020-11-14 13:34:47 +0100

    initial commit

---
+++ src/app/cryptos/cryptos.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CryptosComponent } from './cryptos.component';
+
+describe('CryptosComponent', () => {
+  let component: CryptosComponent;
+  let fixture: ComponentFixture<CryptosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CryptosComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CryptosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
